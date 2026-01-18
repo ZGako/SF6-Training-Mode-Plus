91b6d869276b8f4406323d3d90377ba14da961b4
ZGako <lhgino2003@gmail.com>
2026-01-18 03:02:16 +0100

    Implemented UI part of the Unique Character Gauges, logic still missing

--- reframework/autorun/TrainingModePlusScripts/TrainingParametersPlus.lua
+++ reframework/autorun/TrainingModePlusScripts/TrainingParametersPlus.lua
@@ -12,6 +12,8 @@ module.description = "Module for adjusting training mode parameters. Modifiable 
 module.data = {}
 module.ui = {}
 
+module.data.UniqueCharData = require("TrainingModePlusScripts/UniqueCharacterParametersData")
+
 function module.ui.init(playerUI, playerParam)
 
     --[[
@@ -80,7 +82,6 @@ function module.ui.init(playerUI, playerParam)
     playerUI.super.points = playerParam.SA_Point
     -- changed for super points slider change
     playerUI.super.points_changed = false
-
 
 end
 
@@ -160,6 +161,11 @@ function module.ui.update(playerUI, playerParam)
         need_update = true
     end
 
+    --[[
+        unique character gauges 
+    ]]
+
+    
 
     return need_update
 end
@@ -173,6 +179,8 @@ function module.init()
     module.data.tf_PS = module.data.TrainingManager._tfFuncs._entries[6]:get_field("value")
     module.data.P1Param = module.data.ParameterSetting.PlayerDatas[0]
     module.data.P2Param = module.data.ParameterSetting.PlayerDatas[1]
+    module.data.UniqueGaugeData = module.data.ParameterSetting.UniqueGaugeData
+    module.data.SelectMenu = module.data.TrainingData:get_field("SelectMenu")
     local gBattle = sdk.find_type_definition("gBattle")
     local sPlayer = gBattle:get_field("Player"):get_data(nil)
     local cPlayer = sPlayer.mcPlayer
@@ -184,7 +192,34 @@ function module.init()
     module.ui.p2 = {}
     module.ui.init(module.ui.p1, module.data.P1Param)
     module.ui.init(module.ui.p2, module.data.P2Param)
-
+    -- initialize unique character gauges ui data
+    module.ui.unique = {}
+    -- initialize the unique data based on the required unique data
+    for _, charData in pairs(module.data.UniqueCharData) do
+        module.ui.unique[charData.name] = {}
+        -- initialize timers ui data
+        if charData.timers then
+            for _, timerData in pairs(charData.timers) do
+                module.ui.unique[charData.name][timerData.id] = {}
+                module.ui.unique[charData.name][timerData.id].timer_combo_value = 1
+                module.ui.unique[charData.name][timerData.id].timer_combo_changed = false
+                if timerData.install == true then
+                    module.ui.unique[charData.name][timerData.id].installed_start_value = nil
+                    module.ui.unique[charData.name][timerData.id].installed_start_value_changed = false
+                end
+            end
+        end
+        -- initialize stocks ui data
+        if charData.stocks then
+            for _, stockData in pairs(charData.stocks) do
+                module.ui.unique[charData.name][stockData.id] = {}
+                module.ui.unique[charData.name][stockData.id].stock_slider = 0
+                module.ui.unique[charData.name][stockData.id].stock_slider_changed = false
+                module.ui.unique[charData.name][stockData.id].infinite_checkbox = false
+                module.ui.unique[charData.name][stockData.id].infinite_checkbox_changed = false
+            end
+        end
+    end
 end
 
 function module.on_frame()
@@ -194,6 +229,9 @@ function module.on_frame()
 
     need_update = need_update or module.ui.update(module.ui.p1, module.data.P1Param)
     need_update = need_update or module.ui.update(module.ui.p2, module.data.P2Param)
+
+    -- unique character gauges update
+    
 
     -- updates the training mode immediately rather than waiting for refresh or whatever
     if need_update then
@@ -248,6 +286,59 @@ function module.ui.draw_super(playerUI, playerLabel, playerParam)
     end
 end
 
+function module.ui.draw_unique_character_gauges()
+    local char_id1 = module.data.SelectMenu.PlayerDatas[0].FighterID
+    local char_id2 = module.data.SelectMenu.PlayerDatas[1].FighterID
+    local char_datas
+    if char_id1 == char_id2 then
+        char_datas = { module.data.UniqueCharData[char_id1] }
+    else 
+        char_datas = { module.data.UniqueCharData[char_id1], module.data.UniqueCharData[char_id2] }
+    end
+
+    for _, char_data in pairs(char_datas) do
+        if char_data then
+            imgui.text("Character: " .. char_data.name)
+            -- draw timers
+            if char_data.timers then
+                for _, timerData in pairs(char_data.timers) do
+                    
+                    local ui_timer = module.ui.unique[char_data.name][timerData.id]
+                    descriptor = timerData.descriptors[ui_timer.timer_combo_value]
+                    imgui.text(timerData.name .. ": " .. descriptor)
+
+                    -- use stored slider value as current so it persists
+                    ui_timer.timer_combo_changed, ui_timer.timer_combo_value = imgui.combo(timerData.name .. " Value ", ui_timer.timer_combo_value, timerData.descriptors)
+                    if timerData.install == true and ui_timer.timer_combo_value == 2 then
+                        -- installed timer UI elements
+                        if ui_timer.installed_start_value == nil then ui_timer.installed_start_value = timerData.timerMaxValue end
+                        ui_timer.installed_start_value_changed, ui_timer.installed_start_value = imgui.slider_int(timerData.name .. " starting activation value", ui_timer.installed_start_value, 0, timerData.timerMaxValue)
+                    end
+                end
+            end
+
+            -- draw stocks
+            if char_data.stocks then
+                for _, stockData in pairs(char_data.stocks) do
+                    -- use stored stock ui values
+                    local ui_stock = module.ui.unique[char_data.name][stockData.id]
+                    descriptor = stockData.descriptors[ui_stock.stock_slider + 1]
+                    imgui.text(stockData.name .. ": " .. descriptor)
+
+                    if stockData.allowInfinite then
+                        ui_stock.infinite_checkbox_changed, ui_stock.infinite_checkbox = imgui.checkbox("Toggle Infinite " .. stockData.name, ui_stock.infinite_checkbox)
+                    end
+                    -- if infinite is enabled, don't show the slider
+                    if ui_stock.infinite_checkbox ~= true then
+                        ui_stock.stock_slider_changed, ui_stock.stock_slider = imgui.slider_int(stockData.name .. " Value", ui_stock.stock_slider, stockData.minValue, stockData.maxValue)
+                    end
+                end
+            end
+        end
+        imgui.separator()
+    end
+end
+
 -- UI rendering function
 function module.draw_ui()
     if imgui.collapsing_header("Training Parameters") then
@@ -275,6 +366,13 @@ function module.draw_ui()
             module.ui.draw_super(module.ui.p2, "P2", module.data.P2Param)
             imgui.tree_pop()
         end
+
+        -- Unique Character Gauges
+        if imgui.tree_node("Unique Character Gauges") then
+            module.ui.draw_unique_character_gauges()
+            imgui.tree_pop()
+        end
+
     end
 end
 
---
+++ reframework/autorun/TrainingModePlusScripts/UniqueCharacterParametersData.lua
@@ -0,0 +1,217 @@
+UniqueCharData = {}
+
+UniqueCharData[1] = {
+    name = "Ryu",
+    timers = {{
+        id = "timer_0_001",
+        name = "Denjin Charge",
+        -- if install, then we have a timerMaxValue, otherwise no. Also determines the descriptors
+        install = false,
+        -- for timers, we only have values descriptors of 
+        --[[
+            0 = "Standard",
+            1 = "Maximum" (and "Activated" for Ryu only),
+            2 = "Infinite"
+
+            No reason to bother inserting it here, just do it programmatically later
+        ]]
+    }},
+}
+
+UniqueCharData[21] = {
+    name = "Jamie",
+    timers = {{
+        id = "timer_0_021",
+        name = "The Devil's Song",
+        install = true,
+        timerMaxValue = 900,
+    }},
+    stocks = {{
+        id = "stock_0_021",
+        name = "Drink Level",
+        maxValue = 4,
+        minValue = 0,
+        allowInfinite = false,
+        descriptors = {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        }
+    }}
+}
+
+UniqueCharData[3] = {
+    name = "Kimberly",
+    stocks = {{
+        id = "stock_0_003",
+        name = "Shuriken Bomb Stocks",
+        maxValue = 2,
+        minValue = 0,
+        allowInfinite = true,
+        descriptors = {
+            "2",
+            "1",
+            "0",
+        }
+    }}
+}
+
+UniqueCharData[12] = {
+    name = "Lily",
+    stocks = {{
+        id = "stock_0_012",
+        name = "Windclad Stocks",
+        maxValue = 3,
+        minValue = 0,
+        allowInfinite = true,
+        descriptors = {
+            "0",
+            "1",
+            "2",
+            "3",
+        }
+    }}
+}
+
+UniqueCharData[16] = {
+    name = "Juri",
+    timers = {{
+        id = "timer_0_016",
+        name = "Feng Shui Engine",
+        install = true,
+        timerMaxValue = 600,
+    }},
+    stocks = {{
+        id = "stock_0_016",
+        name = "Fuha Stocks",
+        maxValue = 3,
+        minValue = 0,
+        allowInfinite = true,
+        descriptors = {
+            "0",
+            "1",
+            "2",
+            "3",
+        }
+    }}
+}
+
+UniqueCharData[20] = {
+    name = "E.Honda",
+    stocks = {{
+        id = "stock_0_020",
+        name = "Sumo Spirit",
+        maxValue = 1,
+        minValue = 0,
+        allowInfinite = true,
+        descriptors = {
+            "Standard",
+            "Activated",
+        }
+    }}
+}
+
+UniqueCharData[15] = {
+    name = "Blanka",
+    timers = {{
+        id = "timer_0_015",
+        name = "Lighting Beast",
+        install = true,
+        timerMaxValue = 1500,
+    }},
+    stocks = {{
+        id = "stock_0_015",
+        name = "Blanka-Chan Bomb",
+        maxValue = 3,
+        minValue = 0,
+        allowInfinite = true,
+        descriptors = {
+            "3",
+            "2",
+            "1",
+            "0",
+        }
+    }}
+}
+
+UniqueCharData[18] = {
+    name = "Guile",
+    timers = {{
+        id = "timer_0_018",
+        name = "Solid Puncher",
+        install = true,
+        timerMaxValue = 1500,
+    }}
+}
+
+UniqueCharData[28] = {
+    name = "Mai",
+    stocks = {{
+        id = "stock_0_028",
+        name = "Flame Stocks",
+        maxValue = 5,
+        minValue = 0,
+        allowInfinite = true,
+        descriptors = {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+        }
+    }}
+}
+
+UniqueCharData[30] = {
+    name = "C.Viper",
+    timers = {{
+        id = "timer_0_030",
+        name = "Limit Decoupler",
+        install = true,
+        timerMaxValue = 700,
+    }}
+}
+
+UniqueCharData[5] = {
+    name = "Manon",
+    stocks = {{
+        id = "stock_0_005",
+        name = "Medal Level",
+        maxValue = 4,
+        minValue = 0,
+        allowInfinite = false,
+        descriptors = {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+        }
+    }}
+}
+
+-- iterate through the char data and add timer descriptors
+for _, charData in pairs(UniqueCharData) do
+    if charData.timers then
+        for _, timerData in pairs(charData.timers) do
+            if not timerData.install then
+                timerData.descriptors = {
+                    "Standard",
+                    "Activated",
+                    "Infinite",
+                }
+            else
+                timerData.descriptors = {
+                    "Standard",
+                    "Maximum",
+                    "Infinite",
+                }
+            end
+        end
+    end
+end
+
+return UniqueCharData
