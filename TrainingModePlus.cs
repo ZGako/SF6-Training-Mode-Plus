bfeb1d318d82aa2b15e5b6c306b736f061aacdd0
ZGako <lhgino2003@gmail.com>
2026-09-08 19:35:38 +0200

    Initializing C# branch of the mod.

---
+++ TrainingModePlus.cs
@@ -0,0 +1,21 @@
+﻿namespace SF6_Training_Mode_Plus;
+
+using REFrameworkNET;
+using REFrameworkNET.Attributes;
+
+
+public class TrainingModePlus
+{
+    [PluginEntryPoint]
+    public static void Main()
+    {
+        API.LogInfo("Hello from C#!");
+    }
+
+    [PluginExitPoint]
+    public static void OnUnload()
+    {
+        API.LogInfo("Bye from C#!");
+    }
+
+}
